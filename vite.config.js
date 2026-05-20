422cce6f00c6447b4e767ac058f9e8e96d533ef5
fauzanraf <fauzanraf@users.noreply.github.com>
2026-05-20 16:14:48 +0700

    Fix Vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/field-trip-simulator/",
+  base: process.env.VERCEL === "1" ? "/" : "/field-trip-simulator/",
   server: {
     host: "127.0.0.1",
     port: 5174,
